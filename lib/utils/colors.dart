698d1b62fd79255ca767dbc317cf9326446af8e8
Batuhan Karaoglu <batuhan.karaoglu@ardictech.com>
2023-09-09 14:30:07 +0300

    initial commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color bgColor = const Color(0xFFF7CBFD);
+  static Color bgColor2 = const Color(0xFFB397FD);
+
+  static Color leftGradientColor1 = const Color(0xFFF7CBFD);
+  static Color leftGradientColor2 = const Color(0xFF7758D1);
+  static Color divider = const Color.fromARGB(255, 193, 190, 190);
+
+  static Color mainColor1 = const Color(0xFF1D203E);
+  static Color mainColor2 = const Color(0xFF2C2F48);
+  static Color mainColor3 = const Color(0xFF808080);
+  static Color mainColor4 = const Color(0xFFEDEDED);
+  static Color mainColorBg = const Color(0xFF404040);
+  static Color smoothText = const Color(0xFF808080);
+  static Color playButtonBg = const Color(0xFFA65AF1);
+  static Color playButtonInner = const Color(0xFFF0EFF0);
+  static Color textColor = const Color(0xFF252525);
+}
