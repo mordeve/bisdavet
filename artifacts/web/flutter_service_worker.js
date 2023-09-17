9c7c4c827deb5c41458e2e2108b5a29ce7cd446c
mordeve <mordeve@users.noreply.github.com>
2023-09-17 11:25:31 +0000

    Commit the artifacts.

--- artifacts/web/flutter_service_worker.js
+++ artifacts/web/flutter_service_worker.js
@@ -24,10 +24,10 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "96ae916cd2d1b7320fff853ee
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"index.html": "8b3f98ca5b0031e4b28bbec09ce1e230",
-"/": "8b3f98ca5b0031e4b28bbec09ce1e230",
+"index.html": "9f41658f73d50ca0e7fbe0bc38223e53",
+"/": "9f41658f73d50ca0e7fbe0bc38223e53",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "c6acce0f53127a123c7fdefc448c03d2",
+"main.dart.js": "dbaddfb41cbe5e5c127f27e86764aded",
 "manifest.json": "ea22a49beeec95707eda08d3239a58d0"};
 // The application shell files that are downloaded before a service worker can
 // start.
